024e4628512e0543bf3764b2d23995822ee96fbe
Jack Gallagher <339690067@GoTVDSB.ca>
2024-09-06 09:05:20 -0400

    Finished Assignment

--- Topic 1 - The Basics Assignment/Program.cs
+++ Topic 1 - The Basics Assignment/Program.cs
@@ -9,7 +9,7 @@
 
             Console.WriteLine("hello, " + firstName.ToLower() + " i see that your favorite movie is " + favMovie.ToLower() + "!");
 
-            favMovie = "THE MATRIX";
+            favMovie = favMovie.ToUpper();
 
             Console.WriteLine(favMovie.Contains("THE"));
 
@@ -17,6 +17,34 @@
             favMovie = favMovie.Replace("E", "3");
 
             Console.WriteLine(favMovie);
+
+            string favQuote = "I don't really have a favourite quote from something... Sorry.";
+
+            favQuote = favQuote.ToLower();
+
+            favQuote = favQuote.Replace("a", "");
+            favQuote = favQuote.Replace("e", "");
+            favQuote = favQuote.Replace("i", "");
+            favQuote = favQuote.Replace("o", "");
+            favQuote = favQuote.Replace("u", "");
+            favQuote = favQuote.Replace("y", "");
+
+            Console.WriteLine(favQuote);
+
+            Console.WriteLine();
+
+            Console.Write("                          ");
+            Console.WriteLine("   |\\---/|                ");
+            Console.Write("   / \\__                  ");
+            Console.WriteLine("   | ,_, |                 ");
+            Console.Write("  (    @\\___              ");
+            Console.WriteLine("    \\_`_/-..----.         ");
+            Console.Write("  /         O              ");
+            Console.WriteLine(" ___/ `   ' ,\"\"+ \\  sk  ");
+            Console.Write(" /   (_____/               ");
+            Console.WriteLine("(__...'   __\\    |`.___.';");
+            Console.Write("/_____/   U                ");
+            Console.WriteLine("  (_,...'(_,.`__)/'.....+  ");
         }
     }
 }
