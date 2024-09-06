f39709e979c3a1bf6bf1ee4fb819e95fc697b397
339690067 <339690067@2260I-L29-014>
2024-09-06 08:38:47 -0400

    Add project files.

---
+++ Topic 1 - The Basics Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Topic_1___The_Basics_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Jack";
+            string favMovie = "The Maxrix";
+
+            Console.WriteLine("hello, " + firstName.ToLower() + " i see that your favorite movie is " + favMovie.ToLower() + "!");
+
+            favMovie = "THE MATRIX";
+
+            Console.WriteLine(favMovie.Contains("THE"));
+
+            favMovie = favMovie.Replace("A", "@");
+            favMovie = favMovie.Replace("E", "3");
+
+            Console.WriteLine(favMovie);
+        }
+    }
+}
